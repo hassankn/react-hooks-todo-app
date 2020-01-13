7b8bb0ba40d81c7af6f29685f91b55b38b4e8165
Hassan Khan <40873451+hassankn@users.noreply.github.com>
2020-01-13 15:03:05 -0500

    create function components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Todo({ todo, index }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo'>
+      {todo.text}
     </div>
-  );
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'GoLang',
+      isCompleted: false
+    },
+    {
+      text: 'Redux',
+      isCompleted: false
+    },
+    {
+      text: 'MySQL',
+      isCompleted: false
+    }
+  ]);
+
+  return (
+    <div className='app'>
+      <div className='todo-list'>
+        {todos.map((todo, index) =>
+          <Todo key={index} index={index} todo={todo}>
+
+          </Todo>
+        )}
+      </div>
+    </div>
+  )
+
 }
 
 export default App;
