5f62fee6ea7e5f3d377a4b97c1308bcfd198446e
Hassan Khan <40873451+hassankn@users.noreply.github.com>
2020-01-13 16:40:44 -0500

    add hooks to functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className='todo'>
+    <div style={{
+      textDecoration:
+        todo.isCompleted ? 'line-through' : ''
+    }}
+      className='todo'>
       {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
     </div>
+  )
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' className='input' value={value}
+        placeholder='Add Todo'
+        onChange={e => setValue(e.target.value)} />
+    </form>
   )
 }
 
@@ -25,14 +51,25 @@ function App() {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
   return (
     <div className='app'>
       <div className='todo-list'>
         {todos.map((todo, index) =>
-          <Todo key={index} index={index} todo={todo}>
-
-          </Todo>
+          <Todo key={index} index={index} todo={todo}
+            completeTodo={completeTodo} />
         )}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   )
